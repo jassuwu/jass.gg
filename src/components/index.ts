d319cc5ccfe4c577744ccfc11bc316be51684fc9
jassuwu <jass@jass.gg>
2025-05-22 23:30:36 +0530

    feat: :sparkles: add Projects, Badge, and Footer components to layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
 import Header from "./Header.astro";
 import Hero from "./Hero.astro";
+import Projects from "./Projects.astro";
+import Badge from "./Badge.astro";
+import Footer from "./Footer.astro";
 
-export { Header, Hero };
+export { Header, Hero, Projects, Badge, Footer };
