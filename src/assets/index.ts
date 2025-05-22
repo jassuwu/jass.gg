e661474906b3dd128c722462ea1ba07663460e29
jassuwu <jass@jass.gg>
2025-05-23 00:41:19 +0530

    feat: :sparkles: implement responsive mobile menu and enhance header/footer layout with new assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,6 +6,7 @@ import DISCORD from "./icons/discord.svg";
 import LINKEDIN from "./icons/linkedin.svg";
 import MAIL from "./icons/mail.svg";
 import MOVE_LEFT from "./icons/move-left.svg";
+import HAMBURGER_MENU from "./icons/hamburger-menu.svg";
 
 // GIF
 import SHINJI from "./shinji.gif";
@@ -19,6 +20,7 @@ const ASSETS = {
     LINKEDIN,
     MAIL,
     MOVE_LEFT,
+    HAMBURGER_MENU,
   },
   GIF: {
     SHINJI,
