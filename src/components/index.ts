8ab6b81bb6bb3a146d5568cba5cc9f1ca2e884e6
jassuwu <jass@jass.gg>
2025-06-01 17:13:52 +0530

    feat: :sparkles: add WorkExperiences component and enhance Projects section with GitHub links for improved visibility

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
 import Header from "./Header.astro";
 import Hero from "./Hero.astro";
 import Projects from "./Projects.astro";
+import WorkExperiences from "./WorkExperiences.astro";
 import Badge from "./Badge.astro";
 import Footer from "./Footer.astro";
 import PostHog from "./posthog.astro";
 
-export { Header, Hero, Projects, Badge, Footer, PostHog };
+export { Header, Hero, Projects, WorkExperiences, Badge, Footer, PostHog };
