c647614a04005bb4378f2f2fc63db651fae7eae2
jassuwu <jass@jass.gg>
2026-08-22 19:52:48 +0530

    socials: discord moved domains years ago; stop paying the 301

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const URLS = {
   RESUME: "/resume.pdf",
   TWITTER: "https://x.com/jassdotgg",
   GITHUB: "https://github.com/jassuwu",
-  DISCORD: "https://discordapp.com/users/421489076978450445",
+  DISCORD: "https://discord.com/users/421489076978450445",
   LINKEDIN: "https://www.linkedin.com/in/kprnv",
   EMAIL: "mailto:jass@jass.gg",
 } as const;
